3f2d61ed2be3e98178ad55140a97dc20dd645734
DavidLiuYangg <148336919+DavidLiuYangg@users.noreply.github.com>
2024-05-06 22:01:33 +0200

    Update Projecte.py

--- Projecte.py
+++ Projecte.py
@@ -17,9 +17,13 @@ class Sistema:
         self._datos, self._valoraciones = tipo.llegeix_dades()
         #recomendacion = input("Quin tipus de recomendació es vol?: ")
         
-    def RecomendacioSimple(self): 
-        pass
-    
+    def RecomendacioSimple(self, min_votos):
+        array = np.empty((2,0)) 
+        for i in range(self._valoraciones.shape[1]):
+            datos = np.array([self._valoraciones[:,i][self._valoraciones[:,i] > 0].mean(), sum(self._valoraciones[:,i] > 0)]).reshape(2,1)
+            array = np.append(array, datos, axis=1)
+        avg_global = np.nanmean(array[0,:])
+    return None    
     def RecomendacioColab(self): 
         pass
     
