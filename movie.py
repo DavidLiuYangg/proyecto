6ae40a0a0b9a54bb642cd7385d9ccf90a487186c
Daniela-Lou <146489181+Daniela-Lou@users.noreply.github.com>
2024-05-08 01:14:15 +0200

    Update movie.py

--- movie.py
+++ movie.py
@@ -3,4 +3,17 @@ class Movie:
         self._id = peli_id
         self._titol = titol
         self._generos = generos
+
+    def get_id(self): 
+        return self._id
+    
+    def get_titol(self): 
+        return self._titol
+    
+    def get_generos(self): 
+        return self._generos
+    
+    ID = property(get_id)
+    titol = property(get_titol)
+    generos = property(get_generos)
     
