42045068cf3d6eab35bfa29fc6851ee6136e4398
Daniela-Lou <146489181+Daniela-Lou@users.noreply.github.com>
2024-05-12 04:46:51 +0200

    Delete llibre.py

--- llibre.py
+++
@@ -1,8 +0,0 @@
-class Llibre:
-    def __init__(self, isbn: str, titulo: str, autor: str):
-        self._isbn = isbn
-        self._titulo = titulo
-        self._autor = autor
-        #self._any_publicacio = year
-        #self._editorial = editorial
-        
